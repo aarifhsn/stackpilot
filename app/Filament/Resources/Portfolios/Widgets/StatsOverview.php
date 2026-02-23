ca3f514ef1e05f4bb12b0812db5deae264add3a4
Md. Arif Hassan <aarifhsn@gmail.com>
2026-02-24 00:20:36 +0600

    updated adminpanel widgets

--- app/Filament/Resources/Portfolios/Widgets/StatsOverview.php
+++ app/Filament/Resources/Portfolios/Widgets/StatsOverview.php
@@ -1,16 +1,31 @@
 <?php
 
-namespace App\Filament\Resources\Portfolios\Widgets;
+namespace App\Filament\Widgets;
 
-use Filament\Widgets\StatsOverviewWidget;
+use App\Models\Portfolio;
+use App\Models\Service;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
-class StatsOverview extends StatsOverviewWidget
+class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Published Portfolios', Portfolio::where('is_published', true)->count())
+                ->description(Portfolio::where('is_published', false)->count() . ' drafts')
+                ->color('success')
+                ->icon('heroicon-o-briefcase'),
+
+            Stat::make('Services', Service::where('is_published', true)->count())
+                ->description(Service::where('is_published', false)->count() . ' drafts')
+                ->color('info')
+                ->icon('heroicon-o-wrench-screwdriver'),
+
+            Stat::make('Site Settings', \App\Models\SiteSetting::whereNotNull('value')->where('value', '!=', '')->count() . ' filled')
+                ->description('of ' . \App\Models\SiteSetting::count() . ' total keys')
+                ->color('warning')
+                ->icon('heroicon-o-cog-6-tooth'),
         ];
     }
 }
